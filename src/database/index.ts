0e943b60a838a9002b3881b07b1275999dfd9d46
Robson Locatelli <rlocatelli9@gmail.com>
2023-07-09 13:07:13 -0300

    build: adjusted config knex

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,14 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const knex = setupKnex({
+export const configDatase: Knex.Config = {
   client: 'sqlite',
   connection: {
     filename: './tmp/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './src/database/migrations',
+  },
+}
+export const knex = setupKnex(configDatase)
