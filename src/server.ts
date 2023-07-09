6ddeb67fdf7dc9419d958d555ab509fab2d0d615
Robson Locatelli <rlocatelli9@gmail.com>
2023-07-09 11:34:09 -0300

    build: finished struct inicial's app

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const server = fastify();
+const server = fastify()
 
-
-server.get('/hello', (req, res) => {
-  return 'Hello World!';
+server.get('/hello', () => {
+  return 'Hello World!'
 })
 
-server.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP Server running on port 3333 ⊱🚀')
-})
+server
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running on port 3333 ⊱🚀')
+  })
